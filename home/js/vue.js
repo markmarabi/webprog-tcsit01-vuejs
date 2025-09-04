88f7fc1ee62cbb577fd2c9588b8d50baf3f67d3f
markmarabi <mcmarabi@student.apc.edu.ph>
2025-09-04 09:35:55 +0800

    edited vue

--- home/js/vue.js
+++ home/js/vue.js
@@ -1,11 +1,10 @@
-  const app = Vue.createApp({
-   data() {
+const app = Vue.createApp({
+  data() {
     return {
-     message: "Hello Webprog World! With separate css and js files."
-     vueClass: "pinkBG"
-     
+      message: "Hello Webprog World! With separate css and js files.",
+      vueClass: "pinkBG"
     }
-   }
-  })
-  
-  app.mount('#app')
+  }
+})
+
+app.mount('#app')
