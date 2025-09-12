adc8580b282e20432abadf4f13b5ec3d1b2c156b
markmarabi <mcmarabi@student.apc.edu.ph>
2025-09-12 10:05:04 +0000

    added food item

---
+++ firstsfc/src/components/FoodItem2.vue
@@ -0,0 +1,27 @@
+<template>
+    <div  class="orange">
+      <h2>{{ name }}</h2>
+      <p>{{ message }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Oranges',
+        message: 'I like oranges too'
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  .orange {
+    border: dashed black 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: orange;
+  }
+</style>
